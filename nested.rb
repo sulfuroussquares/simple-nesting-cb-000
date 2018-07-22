45ffc25e9e80f51eab529ae1f7d3874b5fcd3156
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-22 05:16:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto] = {}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = []
-programmer_hash[:yukihiro_matsumoto][:languages].push('LISP', 'C') 
+programmer_hash[:yukihiro_matsumoto][:languages].push("LISP", "C") 
 
 
 		 # add the following information to the top level of programmer_hash
